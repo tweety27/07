5a66409d64fbdb38450bfd772aecc8a99d386f29
tweety27 <135826584+tweety27@users.noreply.github.com>
2023-10-19 15:34:03 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
+int inc(int counter)
+{
+    counter++;
+    return counter;
+}
+
 int main(void)
 {
-    int i;
-    int temp = 1;
-    for(i = 0; i < 5; i++)
-    {
-        printf("temp = %d\n", temp);
-        temp++;
-    }
+    int i = 10;
+    printf("함수 호출 전  i = %d\n", i);
+    inc(i);
+    printf("함수 호출 후  i = %d\n", i);
     return 0;
 }
